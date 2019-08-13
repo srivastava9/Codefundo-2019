4e6454338ed1f1ba8dbaef0edcfd89fe1c72e05f
Rishak Khanna <rishak.khanna@gmail.com>
2019-08-13 16:57:45 +0530

    login,demand,query,login_page-voter/candidate made

---
+++ demands page/demands.js
@@ -0,0 +1,14 @@
+var ques=document.getElementsByClassName('demand_bar');
+var i ;
+//console.log('');
+console.log(ques[1])
+for(i=0;i<5;i++)
+{
+	console.log('s')
+	// if(i%2==1){
+	// 	demandBar[i].style.backgroundColor = 'white';
+	// 	console.log('kuasdg');
+	// }
+	// else console.log('aiaiaia');
+}
+	 
