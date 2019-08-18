4241ad82d4c440e7ef86db01a832436fd5b626f9
Rishak Khanna <rishak.khanna@gmail.com>
2019-08-18 18:58:36 +0530

    changes made in design

--- demands page/demands.js
+++ demands page/demands.js
@@ -1,14 +1,26 @@
-var ques=document.getElementsByClassName('demand_bar');
-var i ;
-//console.log('');
-console.log(ques[1])
-for(i=0;i<5;i++)
-{
-	console.log('s')
-	// if(i%2==1){
-	// 	demandBar[i].style.backgroundColor = 'white';
-	// 	console.log('kuasdg');
-	// }
-	// else console.log('aiaiaia');
+var popup=document.getElementById('popup_background');
+
+function display_popup_background() {
+	popup.style.display = 'block'; 
 }
-	 
+
+window.onclick=function(event){
+	if (event.target==popup) {
+		popup.style.display = 'none';
+	}
+}
+
+function seeMore(){
+	var prev_p=document.activeElement.previousElementSibling;
+	var dots=prev_p.getElementsByClassName("dots")[0];
+	var more=prev_p.getElementsByClassName('more_content')[0];
+	if(dots.style.display = 'inline-block'){
+		document.activeElement.style.display = 'none';
+		dots.style.display = 'none';
+		more.style.display = 'inline-block';
+	}
+	else{
+		dots.style.display = 'inline-block';
+		more.style.display = 'none';
+	}
+}
